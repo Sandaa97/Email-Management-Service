bc9f350c6da6edd0216d795291bdcbde926cf8da
unknown <hsmdesilva97@gmail.com>
2020-12-07 07:26:59 +0530

    Updated responses and added comments

--- routes/emails.js
+++ routes/emails.js
@@ -21,39 +21,42 @@ router.get('/', async(req,res) => {
     }
 });
 /**
- * Get Request for getting one email
+ * Get Request for getting the id and the status of one email
  */
 router.get("/:id", async (req, res) => {
     const email = await Email.findOne({ _id: req.params.id })
-    .then(res.send())
+    .then(res.send({id: req.params.id , status: "Sent"}))
     .catch(res.send({ id: req.params.id , status: "Failed"}))
-	res.send(email)
+	//res.send(email)
 })
 
 /**
- *Post request to creat a new email
+ *Post request to create a new email and getting its status
  */
 router.post('/', async (req, res) => {   
-	const email = new Email({
+	try{const email = new Email({
 		email: req.body.email,
         content: req.body.content,
         subject: req.body.subject
 	})
 	await email.save()
-	res.send(email)
+    res.send({  status: "SENT"})
+    }catch{
+    res.send({  status: "FAILED"})
+}
 });
 
 
 /**
- * Delete request to delete an email
+ * Delete request to delete an email and get its id and status
  */
 router.delete("/:id", async (req, res) => {
 	try {
 		await Email.deleteOne({ _id: req.params.id })
-        res.send()
+        res.send({ id: req.params.id , status: "The email has been successfully Deleted" })
 	} catch {
 		res.status(404)
-		res.send({ id: req.params.id , status: "Failed"})
+		res.send({ id: req.params.id , status: "Failed to delete the email or the email does not exist"})
 	}
 })
 
